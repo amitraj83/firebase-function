c70ae376de2c890d3ade7b3deb8a807f4d7c6c34
Amit Rajq <amitraj.2383@gmail.com>
2020-05-01 18:41:52 +0100

    Admin SDK Working

--- functions/index.js
+++ functions/index.js
@@ -12,14 +12,45 @@ var config = {
     projectId:"qcut-test"
 };
 
+
+
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://qcut-test.firebaseio.com"
+});
 firebase.initializeApp(config);
 
-admin.initializeApp();
+
 var moment = require('moment');
+
+exports.scheduledFunction = functions.pubsub.schedule('every 1 minutes').onRun((context) => {
+
+
+
+
+
 var date = new Date();
-  const today = moment(date).utcOffset('+0100').format('DDMMYYYY');
+const today = moment(date).utcOffset('+0100').format('DDMMYYYY');
+console.log("Today: "+today);
+var db = admin.database();
+var ref = db.ref("/shopDetails");
+
+return ref.once("value", function(snapshot) {
+
+    var a = snapshot.numChildren();
+    console.log("Number of records : "+a);
+    console.log(snapshot.val());
+});
 
 
+
+
+
+});
+/*
 
 exports.waitingQueueUpdate = functions.database.ref("/barberWaitingQueues")
                             .onUpdate((change, context) => {
@@ -38,6 +69,12 @@ exports.waitingQueueUpdate = functions.database.ref("/barberWaitingQueues")
                 barber.forEach((customer) => {
                     console.log("customer key :"+customer.key);
                     shopKey.ref.parent.child("customerView/"+customer.key).set(customer.toJSON());
+                    shopKey.ref.parent.child("customerView/"+customer.key).update({
+                        "shop":shop.key.replace("_"+today, ""),
+                        "barber":barber.key,
+                        "shopWithDate":shopKey,
+
+                    });
                 });
             });
         }
@@ -45,4 +82,33 @@ exports.waitingQueueUpdate = functions.database.ref("/barberWaitingQueues")
      return "Update was called";
 });
 
+exports.waitingQueueWrite = functions.database.ref("/barberWaitingQueues")
+                            .onWrite((change, context) => {
+    console.log('This is the Write' - context.params.pushId);
+     var before = change.before;  // DataSnapshot before the change
+     var shopKey = change.after ; // DataSnapshot after the change
+     console.log("snapshot key :"+shopKey.key);
+    shopKey.forEach((shop) => {
+        console.log(today);
+        console.log("shop key :"+shop.key);
+        console.log("shop key Ends with :"+shop.key.endsWith(today));
+        if (shop.key.endsWith(today) ) {
+            console.log("its today key :"+shop.key);
+            shop.forEach((barber) => {
+                console.log("barber key :"+barber.key);
+                barber.forEach((customer) => {
+                    console.log("customer key :"+customer.key);
+                    shopKey.ref.parent.child("customerView/"+customer.key).set(customer.toJSON());
+                    shopKey.ref.parent.child("customerView/"+customer.key).update({
+                        "shop":shop.key.replace("_"+today, ""),
+                        "barber":barber.key,
+                        "shopWithDate":shopKey,
 
+                    });
+                });
+            });
+        }
+    });
+     return "Write was called";
+});
+*/
