2146b8b35dba1237b7e8397c2c44c0aa2010d9a4
Amit Rajq <amitraj.2383@gmail.com>
2020-05-03 16:57:26 +0100

    Transaction Integrated

--- functions/index.js
+++ functions/index.js
@@ -23,7 +23,7 @@ admin.initializeApp({
 });
 firebase.initializeApp(config);
 
-
+var db = admin.database();
 var moment = require('moment');
 var date = new Date();
 
@@ -34,7 +34,7 @@ exports.scheduledFunction = functions.pubsub.schedule('every 1 minutes').onRun((
 
 var date = new Date();
 const today = moment(date).utcOffset('+0100').format('DDMMYYYY');
-var db = admin.database();
+
 //var ref = db.ref("/Customers/t60nZazByXZczXt1rBat0whcVGJ3/notificationFirebaseToken");
 var ref = db.ref("barberWaitingQueues");
 
@@ -197,28 +197,15 @@ Object.exists = function(obj) {
     return typeof obj !== "undefined" && obj !== null;
 }
 
-function shopDetails() {
-    return new Promise(function(resolve, reject) {
-        var shopInfo = new Array();
-        db.ref("shopDetails").once("value", function(snapshot) {
-            snapshot.forEach((shop) => {
-                shopInfo[shop.key] = shop.child("avgTimeToCut").val();
-            });
-            resolve(shopInfo);
-        });
-    });
-}
 
-function barberWithStatus(promiseReceived) {
+function barberWithStatus(promiseReceived, aShop) {
     
     promiseReceived[1] =  new Promise(function(resolve, reject) {
-                                var statuses = {};
-                                db.ref("barbers").once("value", function(snapshot) {
-                                    snapshot.forEach((shop) => {
-                                         shop.forEach((barber) => {
-                                            statuses[barber.key] = barber.child("queueStatus").val();
-                                         })
-                                     });
+                                var statuses = new Array();
+                                db.ref("barbers/"+aShop.key).once("value", function(snapshot) {
+                                     snapshot.forEach((barber) => {
+                                        statuses[barber.key] = barber.child("queueStatus").val();
+                                     })
                                      resolve(statuses);
                                  });
                                 return reject();
@@ -226,19 +213,20 @@ function barberWithStatus(promiseReceived) {
     return Promise.all(promiseReceived);
 }
 
-function listOfBarberQueues(promiseReceived) {
+function listOfBarberQueues(promiseReceived, aShop) {
 
     promiseReceived[2] = new Promise(function(resolve, reject) {
             db.ref("barberWaitingQueues").once("value", function(snapshot) {
                 var barberQueues = new Array();
                 snapshot.forEach((shop) => {
-                     if (shop.key.endsWith(today) ) {
+                     if (shop.key+"_"+today === aShop.key ) {
                          shop.forEach((barber) => {
-                            var avgTimeToCut = promiseReceived[0][barber.key];
-                            var barberQueue = new BarberQueue(barber.key, shop.key.substring(0, shop.key.indexOf("_")), status, avgTimeToCut);
+                             var avgTimeToCut = promiseReceived[0][barber.key];
+                             var barberQueue = new BarberQueue(barber.key, shop.key.substring(0, shop.key.indexOf("_")), status, avgTimeToCut);
+                             barberQueues.push(barberQueue);
                              barber.forEach((customer) => {
                                     barberQueue.addCustomer(customer);
-                                 barberQueues.push(barberQueue);
+
                              })
                          })
                      }
@@ -251,7 +239,7 @@ function listOfBarberQueues(promiseReceived) {
     return Promise.all([promiseReceived]);
 }
 
-function barberSortedList(promiseReceived) {
+function barberSortedList(promiseReceived, aShop) {
     //Filter out barbers who are stopped
     //return the list of barbers sorted by time to become avaialble
     var barberQueues = promiseReceived[2];
@@ -262,8 +250,8 @@ function barberSortedList(promiseReceived) {
             //Calculate time for a barber to become available
             var remainingMinsToComplete = -1;
             var avgTimeToCut = 15;
-            if(Object.exists(promiseReceived[0][barberQueue.getKey()])) {
-                avgTimeToCut = promiseReceived[0][barberQueue.getKey()];
+            if(Object.exists(aShop.child("avgTimeToCut").val())) {
+                avgTimeToCut = aShop.child("avgTimeToCut").val();
             }
             barberQueue.getCustomers().forEach(customer => {
                 if(customer.child("status").val() === "PROGRESS") {
@@ -286,7 +274,7 @@ function barberSortedList(promiseReceived) {
     return Promise.all(promiseReceived);
 }
 
-function removeAndGetAllCustomerToBeAddedLater(promiseReceived) {
+function removeAndGetAllCustomerToBeAddedLater(promiseReceived, aShop) {
 //All Customer in the queue should be removed and put in a set.
     var allQueuedCustomerSorted = new Array();
     var barberQueues = promiseReceived[2];
@@ -296,7 +284,7 @@ function removeAndGetAllCustomerToBeAddedLater(promiseReceived) {
                 if (Object.exists(customer)) {
                     if (customer.child("status").val() === "QUEUE") {
                         
-                        db.ref("barberWaitingQueues").child(barberQueue.getShopKey()).child(barberQueue.getKey()).child(customer.key).remove()
+                        db.ref("barberWaitingQueues").child(barberQueue.getShopKey()).child(barberQueue.getKey()).child(customer.key).ref.remove()
                         .then(function() {
                             console.log("Remove succeeded.")
                             allQueuedCustomerSorted.push(customer);
@@ -314,7 +302,7 @@ function removeAndGetAllCustomerToBeAddedLater(promiseReceived) {
     return Promise.all(promiseReceived);
 }
 
-function assignCustomersToBarbers(promiseReceived) {
+function assignCustomersToBarbers(promiseReceived, aShop) {
     var sortedListOfCustomer = promiseReceived[4];
     var sortedListOfBarbers = promiseReceived[3];
     var placeInQueue = 1;
@@ -328,140 +316,168 @@ function assignCustomersToBarbers(promiseReceived) {
         }
         if (Object.exists(barberKey) && barberKey !== "") {
             customer.child("timeAdded").set(placeInQueue++);
-            db.ref("barberWaitingQueues").child(currentShopKey).child(barberKey).child(customer.key).set(customer.toJSON())
+            db.ref("barberWaitingQueues").child(aShop.key+"_"+today).child(barberKey).child(customer.key).set(customer.toJSON())
             .then(function() {
                console.log('Customer rellatocation successfully for customer - '+customer.key);
+               sortedListOfBarbers.forEach(sortedBarber => {
+                   if(sortedBarber.getKey() === barberKey) {
+                       sortedBarber.setTimeToGetAvaialble(sortedBarber.getTimeToGetAvaialble() + sortedBarber.getAvgServiceTime());
+                       sortedListOfBarbers = sortedListOfBarbers.sort(CompareForBarberSorted);
+                   }
+               });
                return true;
              })
              .catch(function(error) {
                console.log('Reallocation of customer is failed. Trying again - '+customer.key);
                 db.ref("barberWaitingQueues").child(currentShopKey).child(barberKey).child(customer.key).set(customer.toJSON());
+                sortedListOfBarbers.forEach(sortedBarber => {
+                   if(sortedBarber.getKey() === barberKey) {
+                       sortedBarber.setTimeToGetAvaialble(sortedBarber.getTimeToGetAvaialble() + sortedBarber.getAvgServiceTime());
+                       sortedListOfBarbers = sortedListOfBarbers.sort(CompareForBarberSorted);
+                   }
+               });
              });
-            sortedListOfBarbers.forEach(sortedBarber => {
-                if(sortedBarber.getKey() === barberKey) {
-                    sortedBarber.setTimeToGetAvaialble(sortedBarber.getTimeToGetAvaialble() + sortedBarber.getAvgServiceTime());
-                }
-            });
+
         }
-        sortedListOfBarbers = sortedListOfBarbers.sort(CompareForBarberSorted);
+
     });
     promiseReceived[3] = sortedListOfBarbers;
     
-    return Promise.all(promiseReceived);
+    return Promise.all(db.ref("barberWaitingQueues").child(aShop.key+"_"+today).toJSON());
 }
 
-function updateWaitingTimes(promiseReceived) {
+function updateWaitingTimes(aShopKey) {
     
-    db.ref("barberWaitingQueues").once("value", function(snapshot) {
+    db.ref("barberWaitingQueues/"+aShopKey+"_"+today).once("value", function(shop) {
         
-        snapshot.forEach((shop) => {
-             if (shop.key.endsWith(today) ) {
-                 
-                 new Promise(function (resolve, reject) {
-                     
-                     db.ref("shopDetails/"+shop.key).once("value", function(snapshot) {
-                                return resolve(snapshot.child("avgTimeToCut").val());
-                        });
-                     
-                      return reject();
-                    })
-                    .then(function (avgTimeToCut) {
-                        
-                        shop.forEach((barber) => {
-                            //here we need to update the timings
-                            var inQueueCustomers = new Array();
-                            var inProgressCustomer;
-                            var inProgressCustomerServiceStartTime;
-                            
-                            
-                            barber.forEach((customer) => {
-                                var custStatus = customer.child("status").val();
-                                 if (Object.exists(custStatus) && custStatus === "PROGRESS") {
-                                     inProgressCustomer = customer;
-                                     inProgressCustomerServiceStartTime = customer.child("serviceStartTime").val();
-                                 } else if (Object.exists(custStatus) && custStatus === "QUEUE") {
-                                      inQueueCustomers.push(customer);
-                                 }
-                            })
-                            //Here is the main logic
-                            inQueueCustomers = inQueueCustomers.sort(CustomerComparator);
-                            
-                            
-                            var prevCustomerTime = 0;
-                            for(var i = 0 ; i < inQueueCustomers.length ; i++) {
-                                var customer = inQueueCustomers[i];
-                                var newTimeToWait;
-                                
-                                if ( i === 0 ) {
-                                    //first customer
-                                    if(Object.exists(inProgressCustomer) && Object.exists(inProgressCustomerServiceStartTime)) {
-                                        //first customer in progress
-                                        var currentTimeInMiliSeconds = moment().utcOffset('+0100').valueOf();
-                                        var timeToWait = avgTimeToCut - ((currentTimeInMiliSeconds - inProgressCustomerServiceStartTime)/60000);
-                                        newTimeToWait = Math.max(0, timeToWait);
-                                    } else {
-                                        //first customer not in progress
-                                        newTimeToWait = 0;
-                                    }
-                                    prevCustomerTime = newTimeToWait;
-                                    
-                                } else {
-                                    //Not the first customer
-                                    newTimeToWait = prevCustomerTime + avgTimeToCut;
-                                    prevCustomerTime = newTimeToWait;
-                                    
-                                }
-                                if(Object.exists(newTimeToWait)) {
-                                    customer.ref.child("expectedWaitingTime").set(newTimeToWait);
-                                }
-                            }
-                            
-                            
-                         });
-                    });
-                 
-                 
-                 
-                 
-             }
+         new Promise(function (resolve, reject) {
 
-        });
+             db.ref("shopDetails/"+aShopKey).once("value", function(snapshot) {
+                        return resolve(snapshot.child("avgTimeToCut").val());
+                });
+
+              return reject();
+            })
+            .then(function (avgTimeToCut) {
+
+                shop.forEach((barber) => {
+                    //here we need to update the timings
+                    var inQueueCustomers = new Array();
+                    var inProgressCustomer;
+                    var inProgressCustomerServiceStartTime;
+
+
+                    barber.forEach((customer) => {
+                        var custStatus = customer.child("status").val();
+                         if (Object.exists(custStatus) && custStatus === "PROGRESS") {
+                             inProgressCustomer = customer;
+                             inProgressCustomerServiceStartTime = customer.child("serviceStartTime").val();
+                         } else if (Object.exists(custStatus) && custStatus === "QUEUE") {
+                              inQueueCustomers.push(customer);
+                         }
+                    })
+                    //Here is the main logic
+                    if(Object.exists(inQueueCustomers)) {
+                        inQueueCustomers = inQueueCustomers.sort(CustomerComparator);
+                    }
+
+                    var prevCustomerTime = 0;
+                    for(var i = 0 ; i < inQueueCustomers.length ; i++) {
+                        var customer = inQueueCustomers[i];
+                        var newTimeToWait;
+
+                        if ( i === 0 ) {
+                            //first customer
+                            if(Object.exists(inProgressCustomer) && Object.exists(inProgressCustomerServiceStartTime)) {
+                                //first customer in progress
+                                var currentTimeInMiliSeconds = moment().utcOffset('+0100').valueOf();
+                                var timeToWait = avgTimeToCut - ((currentTimeInMiliSeconds - inProgressCustomerServiceStartTime)/60000);
+                                newTimeToWait = Math.max(0, timeToWait);
+                            } else {
+                                //first customer not in progress
+                                newTimeToWait = 0;
+                            }
+                            prevCustomerTime = newTimeToWait;
+
+                        } else {
+                            //Not the first customer
+                            newTimeToWait = prevCustomerTime + avgTimeToCut;
+                            prevCustomerTime = newTimeToWait;
+
+                        }
+                        if(Object.exists(newTimeToWait)) {
+                            customer.ref.child("expectedWaitingTime").set(newTimeToWait);
+                        }
+                    }
+
+                 });
+            });
+
         resolve(barberQueues);
     });
 
 return true;
 }
 
-new Promise(function (resolve, reject) {
-  return true;
-})
-.then(function () {
-    return shopDetails();
-})
-.then(function () {
-return barberWithStatus();
-})
-.then(function (barbersWithStatuses) {
-//real all data
-return listOfBarberQueues();
-})
-.then(function (listOfBarberQueues) {
-//sortedListOfBarbersForReAllocation
-return barberSortedList();
-})
-.then(function (barberSortedList){
-//removeAndGetAllCustomerToBeAddedLater
-return removeAndGetAllCustomerToBeAddedLater();
-})
-.then(function (allCustomers){
-//assignCustomersToBarbers
-return assignCustomersToBarbers();
-})
-.then(function (barberMap) {
-// updateWaitingTimes
-return updateWaitingTimes();
-})
-.catch(function (error) {
-console.log(error); // 'Some terrrrible error.'
-});
 
+exports.scheduledFunction = functions.pubsub.schedule('every 1 minutes').onRun((context) => {
+//Following should run for each shop
+
+db.ref("shopDetails").once("value", function(snapshot) {
+    snapshot.forEach((aShop) => {
+    var sShopAvgTimeToCut = shop.child("avgTimeToCut").val();
+
+    //transaction starts here on a shop
+    var shopQueuesReference = db.ref("barberWaitingQueues/"+aShop.key+"_"+today);
+    return shopQueuesReference.transaction(function(shopQueuesJSON) {
+        if (shopQueuesJSON === null) {
+        return ; //abort transaction
+        } else {
+            //Performe re-allocation
+            return new Promise(function (resolve, reject) {
+              var promiseReceived = new Array();
+              promiseReceived[0] = true;
+              return promiseReceived;
+            })
+            .then(function (promiseReceived, aShop) {
+            return barberWithStatus(promiseReceived, aShop);
+            })
+            .then(function (promiseReceived, aShop) {
+            //real all data
+            return listOfBarberQueues(promiseReceived, aShop);
+            })
+            .then(function (promiseReceived, aShop) {
+            //sortedListOfBarbersForReAllocation
+            return barberSortedList(promiseReceived, aShop);
+            })
+            .then(function (promiseReceived, aShop){
+            //removeAndGetAllCustomerToBeAddedLater
+            return removeAndGetAllCustomerToBeAddedLater(promiseReceived, aShop);
+            })
+            .then(function (promiseReceived, aShop){
+            //assignCustomersToBarbers
+            return assignCustomersToBarbers(promiseReceived, aShop);
+            })
+            .catch(function (error) {
+            console.log(error); // 'Some terrrrible error.'
+            });
+
+            //transaction should ends here
+            }
+        },
+        function(error, committed, snapshot) {
+          if (error) {
+            console.log('Transaction failed abnormally!', error);
+          } else if (!committed) {
+            console.log('We aborted the transaction.');
+          } else {
+            console.log('Transaction successful. Updating waiting times now');
+            updateWaitingTimes(aShop.key);
+          }
+
+        });
+
+
+        });
+    });
+});
